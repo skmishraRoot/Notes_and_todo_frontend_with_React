52e8ddc1e4726c11028f0d62678a43f2a58ab1ce
Sumit <115337317+skmishraRoot@users.noreply.github.com>
2022-12-23 11:11:33 +0530

    making changes to frontend

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+        <span>Home</span>
+        <span>To-do</span>
+        <span>Notes</span>
+    </div>
+  )
+}
+
+export default Header
